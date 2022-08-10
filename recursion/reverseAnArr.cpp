7bafc1375947d07e2608dc7d035e3883635d1178
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-08-10 10:54:52 +0530

    reverse an array using recursion

--- recursion/reverseAnArr.cpp
+++ recursion/reverseAnArr.cpp
@@ -2,24 +2,34 @@
 using namespace std;
 #define debug(x) cout << #x << " " << x << endl;
 
-///instead of swapping the elements iteratively
-///we are swapping the corner elements and then recursively
-/// calling this function by incrementing the start pointer(lft) 
-///and by decrementing the last pointer(rgt)
-
+/// instead of swapping the elements iteratively
+/// we are swapping the corner elements and then recursively
+///  calling this function by incrementing the start pointer(lft)
+/// and by decrementing the last pointer(rgt)
+int n = 5;
 void reverseArray(int lft, int rgt, int arr[])
 {
     if (lft > rgt)
         return;
-    
-    /// here we are post incrementing and decrementing the pointers itself
-    ///in other way we can just pass the pointers lft and rgt and then while
-    ///calling recursively we can increment and decrement them in function parameters itself
-    ///swap(arr[lft],swap[rgt]);
-    ///reverseArray(lft+1,rgt-1);
 
-    swap(arr[lft++], arr[rgt--]); 
+    /// here we are post incrementing and decrementing the pointers itself
+    /// in other way we can just pass the pointers lft and rgt and then while
+    /// calling recursively we can increment and decrement them in function parameters itself
+    /// swap(arr[lft],swap[rgt]);
+    /// reverseArray(lft+1,rgt-1);
+
+    swap(arr[lft++], arr[rgt--]);
     reverseArray(lft, rgt, arr);
+}
+
+/// we can reverse an array using a single pointer itself as below
+void reverseArrayUsing1pointer(int i, int arr[])
+{
+    if (i > n / 2)
+        return;
+
+    swap(arr[i], arr[n - i - 1]);
+    reverseArrayUsing1pointer(i + 1, arr);
 }
 
 int main()
@@ -33,6 +43,18 @@ int main()
     cout << endl;
     reverseArray(0, 4, arr);
     for (auto i : arr)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    int arr1[] = {11, 12, 13, 14, 15};
+    for (auto i : arr1)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    reverseArrayUsing1pointer(0, arr1);
+    for (auto i : arr1)
     {
         cout << i << " ";
     }
