9c32fba33113b9fc282411aa35b4fb2ebc0e56e5
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-08-19 06:36:18 +0530

    searching an element in nearly sorted array

--- BinarySearch/6modifiedBinsearch.cpp
+++ BinarySearch/6modifiedBinsearch.cpp
@@ -1,3 +1,36 @@
+/*
+Here we have to find an element in nearly sorted array.
+
+Nearly sorted array : In a nearly sorted array , the element which is 
+to be at index i in sorted array may be at i-1 or i or i+1 position.
+
+for example :-  Nearly sorted array: 5 10 30 20 40
+                sorted order of the above array: 5 10 20 30 40
+                from here we can observe that , the element 20 should be at index=2
+                but it is at 2+1= 3 in nearly sorted array which is (index+1) position and also ,
+                the element 30 should be at index=3 but in nearly sorted array
+                it was at 3-1=2 which is index-1,
+*/
+
+
+/*
+Intution of the algorith :-
+
+since the element can be present at i,i-1,i+1 in nearly sorted array,
+we have to calculate the mid and we have to search at mid , mid-1 , mid+1 position
+to find any element.
+1:- If the element at mid is less than search key then we have to make start=mid+2
+and continue to search in right part of mid , here we are doing mid+2 instead of mid+1
+why because , we have already search at mid+1 position, so we are making the start = mid+2 position
+
+2:-If the element at mid is greater than search key then we have to make end=mid-2
+and continue to search in left part of mid.
+
+
+If element not found , we have to return -1.
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int binarySearch(vector<int> v ,int start,int end, int sKey){
@@ -5,8 +38,17 @@ int binarySearch(vector<int> v ,int start,int end, int sKey){
     while(start<=end){
         mid = start + (end-start)/2;
         if(v[mid]==sKey)   return mid;
-        if(v[mid-1]==sKey)   return mid-1;
-        if(v[mid+1]==sKey)   return mid+1;
+
+        /// while checking mid-1 location , we have to 
+        /// verify that whether mid is greater than start or not , 
+        ///if it is start then start-1(Alias mid-1) is not a valid location , so we have to take care of it.
+        if(mid>start && v[mid-1]==sKey)   return mid-1;
+
+        /// here also, we have to check whether mid+1 location is valid or not.
+        /// if mid points to end then mid+1 is not a valid location, so we have 
+        /// to check whether the mid is less than end or not.
+        if(mid<end && v[mid+1]==sKey)   return mid+1;
+
         else if(v[mid]<sKey)   start = mid+2;
         else  end=mid-2;
     }
