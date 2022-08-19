ee28b3992354b330fbdb19150b5ba4348390d24a
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-08-19 13:49:53 +0530

    first and last occurence of an element using stl functions

--- BinarySearch/fl.cpp
+++ BinarySearch/fl.cpp
@@ -1,5 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+/*
+here using lower_bound and upper_bound functions in stl
+we are returning the first and last occurence of given target element 
+in the given array.
+
+
+Here the intution is if we get the lower_bound of a number as last element then
+we can tell that its upper_bound is not present in the array , then we have to 
+return {-1,-1} as our answer otherwise we have to return the 
+respective first and last occurence of the given element.
+*/
 vector<int> searchRange(vector<int>& v, int target) {
     if(v.size()==0) return {-1,-1};
     int lwr = lower_bound(v.begin(),v.end(),target)-v.begin();
@@ -8,8 +20,8 @@ vector<int> searchRange(vector<int>& v, int target) {
     else return {lwr,upr-1};
 }
 int main(){
-    vector<int> v = {2,2};
-    vector<int> res = searchRange(v,3);
+    vector<int> v = {0,1,2,2,2,3,4,5,5,6,6,6};
+    vector<int> res = searchRange(v,4);
     for(auto i:res) cout<<i<<" ";
     return 0;
 }
