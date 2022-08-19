16835074cc4a2f5385c3c5810cef9932ab6306d3
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-08-19 19:55:02 +0530

    finding peak element in an array(may or may not be sorted)

--- BinarySearch/11peakEle.cpp
+++ BinarySearch/11peakEle.cpp
@@ -1,4 +1,29 @@
-//! using Binary search
+/*
+This problem is based on the concept - "Binary search on answer"
+which means , though the array given is not sorted then also there
+is a chance to apply binary search on it.
+
+
+Here the problem is we are given an array(not sorted) and we have 
+to find the peak element in the array .
+we can return any one peak element in the array.
+
+
+criteria for a peak element:- the element should be greater than its neighbours.
+
+Intution is:-
+we face two types here , one are edge cases which means element at 
+first or last index may be the peak element and the other is the element
+in between the first and last elements.
+
+For an element in between first and last can have both neighbour elements,
+but for element at first we should compare it with the right element and if
+it is greater than its right element then we can return it as peak element.
+similarly for an element at the end , we have only left element as neighbour
+so if the left element is smaller ,then we can say the last element is 
+a peak element.
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int binarySearch(vector<int> v ,int start,int end){
@@ -7,19 +32,19 @@ int binarySearch(vector<int> v ,int start,int end){
     while(start<=end){
         mid = start + (end-start)/2;
         if(mid>0 && mid<v.size()-1) {
-            if(v[mid]>v[mid-1] && v[mid]>v[mid+1])
+            if(v[mid]>v[mid-1] && v[mid]>v[mid+1])     //checking for elements between first and last.
                 return mid;
             else if(v[mid-1]>v[mid])
                 end=mid-1;
             else 
                 start=mid+1;
         }
-        else if(mid==0){
+        else if(mid==0){              //checking for element at first index
             if(v[mid]>v[mid+1])
                 return mid;
             return mid+1;
         }
-        else if(mid==v.size()-1){
+        else if(mid==v.size()-1){    //checking for element at last index
             if(v[mid]>v[mid-1])
                 return mid;
             return mid-1;
