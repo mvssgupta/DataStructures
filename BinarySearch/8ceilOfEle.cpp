1f40ce6bc5864522aaf02f8dc3bcd881767e6af5
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-08-19 08:21:12 +0530

    Finding Ceil of an element in given sorted array

--- BinarySearch/8ceilOfEle.cpp
+++ BinarySearch/8ceilOfEle.cpp
@@ -1,18 +1,34 @@
+/*
+here we have to fing ceil of an element.
+Ceil:- Ceil of an element is element itself if it is present in 
+the given array ,
+else the smallest element which is greater than given element
+Eg:- for 5.6 the floor is 5 and ceil is 6 
+lets consider an array :- [1,4,6,8,9] ,
+=> if we search for Ceil(5) 
+then we should get element 6 as answer since 5 is not present in 
+the given array , and the smallest of {6,8,9} is 6 which is greater than 5.
+=>if we search for Ceil(6) 
+then we should get element 6 as answer since it is present in the given array.
+*/
+
+
+//Here the intution is similar to finding the floor of an element.
 #include<bits/stdc++.h>
 using namespace std;
-int res=INT_MAX;
-int binarySearch(vector<int> v ,int start,int end, int sKey){
-    int mid;
+int ceilOfEle(vector<int> v ,int start,int end, int sKey){
+    int mid,res;
+    v[res]=INT_MAX;
     while(start<=end){
         mid = start + (end-start)/2;
         if(v[mid]==sKey)   return mid;
         else if(v[mid]<sKey) start = mid+1;
         else{
-            if(v[mid]<res) res=v[mid];
+            if(v[mid]<v[res]) res=mid;
             end=mid-1;
         }
     }
-    return -1;
+    return res;
 }
 int main(){
      vector<int> v;
@@ -25,8 +41,7 @@ int main(){
     cout<<"Enter the element to find its Ceil:"; 
     int skey; 
     cin>>skey;
-    int result = binarySearch(v,0,v.size()-1,skey);
-    if(result!=-1) cout<<"Ceil of  "<<skey<<" is " <<v[result]<<endl;
-    else cout<<"Ceil of "<<skey<<" is "<<res<<endl;
+    int result = ceilOfEle(v,0,v.size()-1,skey);
+    cout<<"Ceil of  "<<skey<<" is " <<v[result]<<endl;
     return 0;
 }
