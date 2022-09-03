a27359f411867e8b4fc54ad27e08b475f0a2eb56
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-09-03 09:54:56 +0530

    clockwise rotation of array by n elements

---
+++ arrays/arrayRotation.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << #x << " " << x << endl;
+
+//rotating the array in clockwise
+
+vector<int> rotateArray(vector<int> &v,int n)
+{
+    while(n--)
+    {
+        int last = v[v.size()-1];
+        for(int i=v.size()-2;i>=0;i--)
+        {
+            v[i+1]=v[i];
+        }
+        v[0] = last;
+    }
+    return v;
+}
+
+int main()
+{
+    vector<int> v = {3,2,1,5,7,8};
+    cout<<"Before rotation : ";
+    for(auto i:v)
+    {
+        cout<<i<<" ";
+    }cout<<endl;
+    cout<<"Enter the number of times the array need to be rotated : ";
+    int n;
+    cin>>n;
+    v = rotateArray(v,n);
+    cout<<"After rotation : ";
+    for(auto i:v)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
