7f4ff144e8c73bbb684f2d2ea36cbd63371a46af
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-09-13 18:39:01 +0530

    print array in reverse order using recursion

---
+++ recursion/printArrReverse.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define debug(x) cout<<#x<<" "<<x<<endl;
+
+void rec(vector<int> &v , int ind)                               
+{
+    if(ind==v.size()) return;
+    rec(v,ind+1);
+    cout<<v[ind]<<" ";
+}
+int main()
+{
+    vector<int> v={1,2,3,4,5};
+    rec(v,0);
+    return 0;
+}
